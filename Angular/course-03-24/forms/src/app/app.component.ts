ac5098b95f2e4a56dc8306be02250c5ee98d3f88
bnto <bnto@users.noreply.github.com>
2024-03-04 13:23:18 +0100

    Add template form component

--- Angular/course-03-24/forms/src/app/app.component.ts
+++ Angular/course-03-24/forms/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TemplateFormComponent } from './template-form/template-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, TemplateFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ Angular/course-03-24/forms/src/app/template-form/template-form.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './template-form.component.html',
+  styleUrl: './template-form.component.css'
+})
+export class TemplateFormComponent {
+
+  getNinja(id: number) {
+
+  }
+}
