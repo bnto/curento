d50f6fc92aa22288df279ee2fa8ab55ee9098559
bnto <bnto@users.noreply.github.com>
2024-03-04 15:33:30 +0100

    Subscribe to the ninja service method and display results

--- Angular/course-03-24/forms/src/app/template-form/template-form.component.ts
+++ Angular/course-03-24/forms/src/app/template-form/template-form.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
+import { Ninja } from '../models/ninja';
+import { NinjaService } from '../ninja.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-template-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './template-form.component.html',
   styleUrl: './template-form.component.css'
 })
 export class TemplateFormComponent {
+  ninjas!: Ninja[];
 
-  getNinja(id: number) {
-
+  constructor(private ninja_service: NinjaService) {
+    this.ninja_service.getNinjas().subscribe( (data: Ninja[]) => {
+      this.ninjas = data;
+    })
   }
 }
