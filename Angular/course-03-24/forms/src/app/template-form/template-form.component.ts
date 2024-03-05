bbce23535c7f5cf672e26b6fcb42907c39e7517e
bnto <bnto@users.noreply.github.com>
2024-03-05 13:30:54 +0100

    Add hardcoded example form

--- Angular/course-03-24/forms/src/app/template-form/template-form.component.ts
+++ Angular/course-03-24/forms/src/app/template-form/template-form.component.ts
@@ -2,20 +2,27 @@ import { Component } from '@angular/core';
 import { Ninja } from '../models/ninja';
 import { NinjaService } from '../ninja.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-template-form',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './template-form.component.html',
   styleUrl: './template-form.component.css'
 })
 export class TemplateFormComponent {
   ninjas!: Ninja[];
+  ninja!: Ninja;
 
   constructor(private ninja_service: NinjaService) {
     this.ninja_service.getNinjas().subscribe( (data: Ninja[]) => {
       this.ninjas = data;
     })
+    this.ninja = {
+      id: 5,
+      name: 'okey',
+      belt: 'red'
+    }
   }
 }
