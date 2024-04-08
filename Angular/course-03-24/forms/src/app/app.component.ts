48bfa64ee6f8304bb2fa512d21db4ded58b98094
bnto <bnto@users.noreply.github.com>
2024-04-08 10:00:04 +0200

    Hook simple nav with routing

--- Angular/course-03-24/forms/src/app/app.component.ts
+++ Angular/course-03-24/forms/src/app/app.component.ts
@@ -1,15 +1,34 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { TemplateFormComponent } from './template-form/template-form.component';
+
+interface Nav {
+  href: string,
+  name: string,
+  exact: boolean,
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, TemplateFormComponent],
+  imports: [CommonModule, RouterOutlet, TemplateFormComponent, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'forms';
+
+  nav: Nav[] = [
+    {
+      href: '/',
+      name: 'Index',
+      exact: true,
+    },
+    {
+      href: 'home',
+      name: 'Home',
+      exact: true,
+    }
+  ]
 }
