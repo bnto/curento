8e879489ed6013dff84081b669a2c9c8f58ac6c7
bnto <bnto@users.noreply.github.com>
2024-04-19 11:18:45 +0200

    Add comments for React.createElement() and build a custom render

--- React/joyofreact/src/index.js
+++ React/joyofreact/src/index.js
@@ -4,12 +4,16 @@ import { createRoot } from 'react-dom/client';
 
 // 2. Create a React element
 const element = React.createElement(
-    'p',
-    { id: 'hello' },
-    'Hello World!'
-);
+    'p', // type of the element to create
+    { id: 'hello' }, // properties of the element
+    'Hello World!' // content of the element
+); // returns a react element
 
 // 3. Render the application
 const container = document.querySelector('#root');
 const root = createRoot(container);
 root.render(element);
+
+/* NOTE: old way of rendering a react app prior v18: */
+// import ReactDOM from 'react-dom';
+// ReactDOM.render(element,container);
